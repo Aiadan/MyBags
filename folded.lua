12639633e113051565e749b30d3dbb0df3b37c77
MyGamesDevelopmentAcc <116946134+MyGamesDevelopmentAcc@users.noreply.github.com>
2025-02-01 10:08:52 +0100

    Resolved issue with folding not storing properly and breaking for unassigned category

--- folded.lua
+++ folded.lua
@@ -1,6 +1,7 @@
 local addonName, AddonNS = ...
 
 local FOLDED_CHANGED = AddonNS.Const.Events.FOLDED_CHANGED;
+local UNASSIGNE_CATEGORY_DB_STORAGE_NAME=AddonNS.Const.UNASSIGNE_CATEGORY_DB_STORAGE_NAME
 AddonNS.Folded ={}
 AddonNS.Folded.foldedMap = {}
 function AddonNS.Folded:OnInitialize()
@@ -8,13 +9,16 @@ function AddonNS.Folded:OnInitialize()
     AddonNS.Folded.foldedMap = AddonNS.db.foldedCategories
 end
 AddonNS.Events:OnInitialize(AddonNS.Folded.OnInitialize)
-
+local function getCategorySafeNameForStorage(category)
+    return category.name or UNASSIGNE_CATEGORY_DB_STORAGE_NAME;
+end
 function AddonNS.Folded.isFolded(category)
-    return AddonNS.Folded.foldedMap[category.name];
+    return AddonNS.Folded.foldedMap[getCategorySafeNameForStorage(category)];
 end
 
 function AddonNS.Folded.toggleFolding(category)
-    AddonNS.Folded.foldedMap[category.name] = not AddonNS.Folded.foldedMap[category.name] ;
-    AddonNS.printDebug("toggleFolding", category and category.name);
+    local name = getCategorySafeNameForStorage(category);
+    AddonNS.Folded.foldedMap[name] = not AddonNS.Folded.foldedMap[name] ;
+    AddonNS.printDebug("toggleFolding", name);
     AddonNS.Events:TriggerCustomEvent(FOLDED_CHANGED, category);
 end
